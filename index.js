1a4b40e7fc3ddc88578a8ab8a433168a979bdfd2
samir-207 <mirzashikoh93@gmail.com>
2024-03-23 15:07:02 +0500

    formatted code

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+#! usr/bin/env node
 import inquirer from "inquirer";
 // >tasks<
 // 1.Let computer generate a random number.
 // 2.Ask user to guess a number.
 // 3.Compare both numbers and show result.
 const randNumb = Math.floor(Math.random() * 10 + 1);
-const ans = await inquirer.prompt([{
+const ans = await inquirer.prompt([
+    {
         message: "Guess a number b/w 1-10 =",
         type: "number",
         name: "numbGuessed",
-    }
+    },
 ]);
 if (ans.numbGuessed === randNumb) {
     console.log("CONGRATS!!! YOU GUESSED THE SAME COMPUTER DID");
